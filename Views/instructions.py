c6e866e27d7f570a8c8803ddecb9e327d2dcaf37
FinnellSteven <steven.finnell13@gmail.com>
2023-09-18 16:12:10 -0400

    Simplified main and added in separate views for particular elements. Still need to build out lvl select.

---
+++ Views/instructions.py
@@ -0,0 +1,51 @@
+import arcade, arcade.gui
+from Views.game import GameView
+
+class InstructionsView(arcade.View):
+    def __init__(self, screen_width, screen_height):
+        super().__init__()
+
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        self.window.set_mouse_visible(True)
+        self.window.set_location(int((arcade.get_display_size()[0] - screen_width) / 2),
+                          int((arcade.get_display_size()[1] - screen_height) / 2))
+
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.ORANGE_PEEL)
+
+        red_style = {
+            "font_name": ("calibri", "arial"),
+            "font_size": 15,
+            "font_color": arcade.color.WHITE,
+            "border_width": 2,
+            "border_color": None,
+            "bg_color": arcade.color.REDWOOD,
+            "bg_color_pressed": arcade.color.WHITE,
+            "border_color_pressed": arcade.color.RED,  # also used when hovered
+            "font_color_pressed": arcade.color.RED,
+        }
+
+        self.v_box = arcade.gui.UIBoxLayout(space_between=20)
+
+        title_label = arcade.gui.UILabel(text="Instructions", width=400, height=50, font_size=18, font_name=("calibri", "arial"), text_color=arcade.color.BLACK, align="center")
+        begin_button = arcade.gui.UIFlatButton(text="Begin", width=200, style=red_style)
+
+        begin_button.on_click = self.on_click_begin
+
+        self.v_box.add(title_label)
+        self.v_box.add(begin_button)
+
+        self.manager.add(arcade.gui.UIAnchorWidget(anchor_x="center_x", anchor_y="center_y", child=self.v_box))
+
+    def on_click_begin(self, event):
+        game_view = GameView(self.screen_width, self.screen_height)
+        self.window.show_view(game_view)
+
+    def on_draw(self):
+        self.clear()
+        self.manager.draw()
--- main.py
+++ main.py
@@ -1,115 +1,14 @@
 import arcade
-import random
-from player import Player
-from enemies import Drone
+from Views.menu import MenuView
 
 screen_width = 1024
 screen_height = 768
 game_title = 'Dark Matter'
 
-class MyGame(arcade.Window):
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
-        self.set_mouse_visible(False)
-        self.set_location(int((arcade.get_display_size()[0] - screen_width) / 2),
-                          int((arcade.get_display_size()[1] - screen_height) / 2))
-        # idle at start
-        self.left_pressed = False
-        self.right_pressed = False
-        self.up_pressed = False
-        self.down_pressed = False
-        self.setup()  # call the setup/new game function
-
-    def setup(self):
-        self.player = Player(screen_width, screen_height)
-        self.enemy_list = arcade.SpriteList()
-
-        for i in range(0, 10):
-            drone = Drone(10, 1)
-            drone.center_x = random.randrange(screen_width)
-            drone.center_y = random.randrange(screen_height)
-
-            self.enemy_list.append(drone)
-
-    def on_draw(self):
-        arcade.start_render()
-        self.clear()
-        self.enemy_list.draw()
-        self.player.draw()
-
-    def update(self, delta_time):
-        self.player.update()
-        self.enemy_list.update()
-
-        for enemy in self.enemy_list:
-            enemy.follow_sprite(self.player)
-
-        hit_list = arcade.check_for_collision_with_list(self.player, self.enemy_list)
-
-        for enemy in hit_list:
-            enemy.remove_from_sprite_lists()
-            self.player.hull -= 1
-
-        if self.player.hull == 0:
-            arcade.close_window()
-
-        # Add friction
-        if self.player.change_x > self.player.friction:
-            self.player.change_x -= self.player.friction
-        elif self.player.change_x < -self.player.friction:
-            self.player.change_x += self.player.friction
-        else:
-            self.player.change_x = 0
-
-        if self.player.change_y > self.player.friction:
-            self.player.change_y -= self.player.friction
-        elif self.player.change_y < -self.player.friction:
-            self.player.change_y += self.player.friction
-        else:
-            self.player.change_y = 0
-
-        # Apply acceleration based on the keys pressed
-        if self.up_pressed and not self.down_pressed:
-            self.player.change_y += self.player.acceleration_rate
-        elif self.down_pressed and not self.up_pressed:
-            self.player.change_y += -self.player.acceleration_rate
-        if self.left_pressed and not self.right_pressed:
-            self.player.change_x += -self.player.acceleration_rate
-        elif self.right_pressed and not self.left_pressed:
-            self.player.change_x += self.player.acceleration_rate
-
-        if self.player.change_x > self.player.max_speed:
-            self.player.change_x = self.player.max_speed
-        elif self.player.change_x < -self.player.max_speed:
-            self.player.change_x = -self.player.max_speed
-        if self.player.change_y > self.player.max_speed:
-            self.player.change_y = self.player.max_speed
-        elif self.player.change_y < -self.player.max_speed:
-            self.player.change_y = -self.player.max_speed
-
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.W:
-            self.up_pressed = True
-        elif key == arcade.key.S:
-            self.down_pressed = True
-        elif key == arcade.key.A:
-            self.left_pressed = True
-        elif key == arcade.key.D:
-            self.right_pressed = True
-
-    def on_key_release(self, key, modifiers):
-        if key == arcade.key.W:
-            self.up_pressed = False
-        elif key == arcade.key.S:
-            self.down_pressed = False
-        elif key == arcade.key.A:
-            self.left_pressed = False
-        elif key == arcade.key.D:
-            self.right_pressed = False
-
 def main():
-    game = MyGame(screen_width, screen_height, game_title)
-    game.setup()
+    window = arcade.Window(screen_width, screen_height, game_title)
+    menu_view = MenuView(screen_width, screen_height)
+    window.show_view(menu_view)
     arcade.run()
 
 if __name__ == '__main__':
